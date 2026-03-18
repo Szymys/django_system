608623ab567bee9b496b18aeae11e2af4203611d
Szymon <szymon.mysliwiec@gmail.com>
2026-03-18 18:42:24 +0100

    poprawki , dziala lokalnie

--- ecommerce/core/admin.py
+++ ecommerce/core/admin.py
@@ -6,17 +6,20 @@ from .models import StoreSettings
 
 @admin.register(StoreSettings)
 class StoreSettingsAdmin(admin.ModelAdmin):
-    list_display = ("store_name",)
-    fields = ("store_name", "store_description")
+    list_display = ("store_name", "navbar_color")
+    fields = ("store_name", "store_description", "navbar_color")
+
+
 
     def has_add_permission(self, request):
+
         # pozwala dodac tylko jeden rekord
         if StoreSettings.objects.exists():
             return False
         return super().has_add_permission(request)
 
 
-
+# opcjonalnie blokujemy usuwanie
     def has_delete_permission(self, request, obj=None):
-        # opcjonalnie blokujemy usuwanie
+        
         return False
